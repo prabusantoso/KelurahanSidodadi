262d56eb7e6dd54949d82d4fe22ff005a890f317
prabusantoso <prabusantoso95@gmail.com>
2018-12-13 16:12:11 +0700

    Update fix

--- app.js
+++ app.js
@@ -47,8 +47,11 @@ app.use('/pengumumans', pengumumanRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -59,6 +62,13 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// change bin/www to here
+const port = process.env.PORT || 4000;
+
+app.listen(port, function() {
+  console.log(`Server Starts on ${port}`);
 });
 
 module.exports = app;
