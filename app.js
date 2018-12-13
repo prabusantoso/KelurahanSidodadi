8806c3e38ce295a36c93d5c5f783a5b1c5d7ef09
prabusantoso <prabusantoso95@gmail.com>
2018-12-13 15:53:56 +0700

    update new

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret : 'keyboard cat', cookie: { }}));
+app.use(session({secret : 'keyboard cat', cookie: { },resave: true,saveUninitialized: true}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
